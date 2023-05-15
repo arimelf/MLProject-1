c893088efecd9056d4ee3b7e7b2d072472e750b4
arimelf <eedeverdeen@gmail.com>
2023-05-14 21:03:31 -0700

    data transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -3,11 +3,14 @@ import sys
 import pandas as pd
 
 sys.path.append("/Users/curtiswu/Documents/GitHub/MLProject-1/src")
+sys.path.append("/Users/curtiswu/Documents/GitHub/MLProject-1/src/components")
 
 from exception import CustomException
 from logger import logging
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from data_transformation import DataTransformation
+from data_transformation import DataTransformationConfig
 
 @dataclass #can directly define class variable without __init__
 class DataIngestionConfig:
@@ -47,4 +50,9 @@ class DataIngestion:
 
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
+
+
